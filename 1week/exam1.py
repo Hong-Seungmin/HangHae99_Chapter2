c2fe5d55fca86f222baab0adffe079fddf12175e
hongseungmin <globalsh1@gmail.com>
2022-03-17 18:57:06 +0900

    시험문제 재풀이, 모범답안 이해한 뒤 다시 해봄

--- 1week/exam1.py
+++ 1week/exam1.py
@@ -1,70 +1,91 @@
 # https://programmers.co.kr/learn/courses/30/lessons/42583?language=python3
 
+import collections
+
+
 # 한번에 10kg 까지만 지나갈 수 있는 다리를 트럭들이 지나가야하는 총 시간은 얼마인가?
 # bridge_length : 다리의 길이, 다리의 칸 수
 # weight : 다리가 버틸 수 있는 무게 , weight 이상의 무게는 지나갈 수 없다.
 # truck_weights : 각 트럭의 무게
-import collections
-
 
 def solution(bridge_length, weight, truck_weights):
     answer = 0
-
-    waiting = truck_weights[:]  # 대기 중인 트럭
-    moving = []  # 지나가는 중인 트럭
-    finish = []  # 다리를 건넌 트럭
-    count = []  # 각 차량의 건너는 시간 표시
-
-    while len(finish) < len(truck_weights):  # 모든 트럭이 통과할때까지
-        answer += 1
-
-        # 대기 중인 차량의 건널 여부 판단 후 처리
-        if waiting:
-            waiting_front_truck = waiting[0]
-            # 다리를 건너는 차량의 총 무게와 가장 앞 대기차량의 무게가 다리 부하를 넘지 않으면 이동시킨다.
-            if sum(moving) + waiting_front_truck <= weight:
-                # print('moving', waiting[0])
-                moving.append(waiting.pop(0))
-                count.append(1)
-
-        # 이동 중인 차량의 통과 여부 확인
-        for i, k in enumerate(moving):
-            if count[i] == bridge_length:
-                # print('finish', k)
-                finish.append(k)
-                moving.pop(i)
-                count.pop(i)
-            else:
-                count[i] += 1
-
-    return answer
-
-
-# 5, 10, [7, 4, 5, 6]
-def solution(bridge_length, weight, truck_weights):
-    answer = 0
-
-    # [7,0,0 ,0 ,0]
-    bridge = collections.deque([0] * bridge_length)
+    bridge = [0] * bridge_length  # 다리에 있는 차량 정보
+    time = 0
+    total_bridge_weight = 0
 
     while bridge:
-        answer += 1
-        bridge.popleft() # ['0',]
-        if truck_weights:  # [7, 4, 5, 6]
-            if sum(bridge) + truck_weights[0] <= weight:
-                # [0, 7] , [4, 5, 6]
-                bridge.append(truck_weights.pop(0)) # [6,]
+        time += 1
+        truck = bridge.pop(0)  # 도착한 차량은 다리에서 제거, 다리 위의 차량을 이동
+        total_bridge_weight -= truck  # 다리를 벗어난 트럭의 무게를 뺀다.
+
+        # 트럭이 있다면, 트럭을 이동시킨다.
+        if truck_weights:
+            # 다리가 버틸 수 있는 무게라면 트럭을 다리로 보낸다.
+            if truck_weights[0] + total_bridge_weight <= weight:
+                truck = truck_weights.pop(0)
+                bridge.append(truck)  # 가장앞에서 대기중인 트럭을 다리로 보냄
+                total_bridge_weight += truck  # 트럭의 무게를 더한다.
             else:
-                # [7, 0]
-                bridge.append(0) #[7,0]
+                bridge.append(0)  # 무게가 초과한다면, 다리 위에 있는 차량만 이동시킨다.
+
+    answer = time
     return answer
 
-'''
-<<<-----
-end [0,0,0,0,0,0] start
-'''
+# def solution(bridge_length, weight, truck_weights):
+#     answer = 0
+#
+#     waiting = truck_weights[:]  # 대기 중인 트럭
+#     moving = []  # 지나가는 중인 트럭
+#     finish = []  # 다리를 건넌 트럭
+#     count = []  # 각 차량의 건너는 시간 표시
+#
+#     while len(finish) < len(truck_weights):  # 모든 트럭이 통과할때까지
+#         answer += 1
+#
+#         # 대기 중인 차량의 건널 여부 판단 후 처리
+#         if waiting:
+#             waiting_front_truck = waiting[0]
+#             # 다리를 건너는 차량의 총 무게와 가장 앞 대기차량의 무게가 다리 부하를 넘지 않으면 이동시킨다.
+#             if sum(moving) + waiting_front_truck <= weight:
+#                 # print('moving', waiting[0])
+#                 moving.append(waiting.pop(0))
+#                 count.append(1)
+#
+#         # 이동 중인 차량의 통과 여부 확인
+#         for i, k in enumerate(moving):
+#             if count[i] == bridge_length:
+#                 # print('finish', k)
+#                 finish.append(k)
+#                 moving.pop(i)
+#                 count.pop(i)
+#             else:
+#                 count[i] += 1
+#
+#     return answer
+#
+#
+# # 5, 10, [7, 4, 5, 6]
+# def solution(bridge_length, weight, truck_weights):
+#     answer = 0
+#
+#     # [7,0,0 ,0 ,0]
+#     bridge = collections.deque([0] * bridge_length)
+#
+#     while bridge:
+#         answer += 1
+#         bridge.popleft() # ['0',]
+#         if truck_weights:  # [7, 4, 5, 6]
+#             if sum(bridge) + truck_weights[0] <= weight:
+#                 # [0, 7] , [4, 5, 6]
+#                 bridge.append(truck_weights.pop(0)) # [6,]
+#             else:
+#                 # [7, 0]
+#                 bridge.append(0) #[7,0]
+#     return answer
+
 
 #
-# print(solution(2, 10, [7, 4, 5, 6]))
-# print(solution(100, 100, [10]))
+print(solution(2, 10, [7, 4, 5, 6]))
+print(solution(100, 100, [10]))
 print(solution(100, 100, [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]))
--- 1week/exam2.py
+++ 1week/exam2.py
@@ -2,6 +2,47 @@
 import math
 
 
+# 모범답안을 보고 새로 작성
+
+# 모든 작업은 동시에 진행된다.
+# progresses는 완성도를 의미하며, 100이되면 배포할 수 있는 상태가 된다.
+# sppeds는 하루에 할 수 있는 완성도이다.
+# 각 배포의 우선순위는 앞에서부터 뒤로갈수록 낮아진다.
+# 즉, 앞 작업이 덜 끝났으면 뒷 작업은 배포를 할 수 없다.
+# 각 리스트의 동일 인덱스끼리 매칭된다.
+# answer에는 배포를 할 수 있는 날을 기준으로 그날 몇개의 작업을 배포하였는지 보관한다.
+
+# 예) [95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]
+#      ==> [1, 3, 2]  ## 작업일 상관없이 배포 순서 기준 [1개 배포, 3개 배포, 2개 배포]
+def solution(progresses, speeds):
+    answer = []
+    day = 0  # 작업에 걸린 기간
+    cnt = 0  # 한번에 배포할 수 있는 배포 카운트
+
+    while progresses:
+        # day += 1  # 하루에 n개의 작업을 완성할 수 있으니, 여기서 하루를 넘기면 안된다.
+
+        # 완성도가 100이 넘으면 배포 카운트를 증가시킨다.
+        if progresses[0] + (day * speeds[0]) >= 100:
+            # 완성한 작업물 배포 준비 (배포 카운트 증가)
+            cnt += 1
+
+            # 완성된 작업을 목록에서 제거한다.
+            progresses.pop(0)
+            speeds.pop(0)
+        else:
+            # 이전 작업물에 대해서 배포 준비가 끝났다면 배포한다.
+            if cnt > 0:  # 배포할것이 있다면,
+                answer.append(cnt)  # 배포할 작업 갯수를 넣는다.
+                cnt = 0  # 배포 등록하였으니, 다음 배포 카운트를 준비한다.
+            day += 1  # 미완성된 (우선 작업)이 있다면, 하루를 넘긴다.
+
+    # 마지막 작업이 완료되면, answer.append(cnt)를 안하고 반복문이 종료되니,
+    # 마지막 배포에 대해서 수동으로 등록해준다.
+    answer.append(cnt)
+    return answer
+
+
 #
 #
 # def solution(progresses, speeds):
@@ -38,64 +79,64 @@ import math
 
 
 # [95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]
-def solution(progresses, speeds):
-    answer = []
-    job_days = []
-
-    # job_days = [5, 10, 1, 1, 20, 1]
-    for i, item in enumerate(progresses):
-        days = math.ceil((100 - item) / speeds[i])
-        job_days.append(days)
-
-    cnt = 0 # 배포 갯수
-
-    # 모든 잡이 끝날때까지
-    while job_days:
-        cnt += 1
-        job = job_days.pop(0)
-
-        # 잡이 있으면서, 다음 잡보다 더 걸리면
-        while job_days and job >= job_days[0]:
-            cnt += 1
-            job = job_days.pop(0)
-
-        answer.append(cnt)
-        cnt = 0
-
-    return answer
-
-
-# [93, 30, 55], [1, 30, 5]
-def solution(progresses, speeds):
-    answer = []
-    time = 0  # 작업 시간(일), 모든 작업이 동시에 진행하므로 매일 누적된다.
-    count = 0  # 일일 배포 갯수
-
-    while len(progresses) > 0:
-
-        # 작업이 완료된것이 있다면, 배포 카운트를 올린다.
-        if (progresses[0] + time * speeds[0]) >= 100:
-
-            # 완료한것은 작업목록에서 제외
-            progresses.pop(0)
-            speeds.pop(0)
-
-            # 배포 카운트 증가
-            # 오늘 다른 작업물의 완료상태를 확인해야하니 time은 증가하지 않는다.
-            count += 1
-
-        # 현재 작업이 완료되지 않았다면,
-        else:
-            # 이전에 완료한 작업이 있으면 배포한다.
-            if count > 0:
-                answer.append(count)
-                count = 0 # 배포하였으니 다시 카운트는 0으로 설정
-
-            # 오늘 작업량에 대한 하루 증가
-            time += 1
-
-    answer.append(count)
-    return answer
+# def solution(progresses, speeds):
+#     answer = []
+#     job_days = []
+#
+#     # job_days = [5, 10, 1, 1, 20, 1]
+#     for i, item in enumerate(progresses):
+#         days = math.ceil((100 - item) / speeds[i])
+#         job_days.append(days)
+#
+#     cnt = 0 # 배포 갯수
+#
+#     # 모든 잡이 끝날때까지
+#     while job_days:
+#         cnt += 1
+#         job = job_days.pop(0)
+#
+#         # 잡이 있으면서, 다음 잡보다 더 걸리면
+#         while job_days and job >= job_days[0]:
+#             cnt += 1
+#             job = job_days.pop(0)
+#
+#         answer.append(cnt)
+#         cnt = 0
+#
+#     return answer
+#
+#
+# # [93, 30, 55], [1, 30, 5]
+# def solution(progresses, speeds):
+#     answer = []
+#     time = 0  # 작업 시간(일), 모든 작업이 동시에 진행하므로 매일 누적된다.
+#     count = 0  # 일일 배포 갯수
+#
+#     while len(progresses) > 0:
+#
+#         # 작업이 완료된것이 있다면, 배포 카운트를 올린다.
+#         if (progresses[0] + time * speeds[0]) >= 100:
+#
+#             # 완료한것은 작업목록에서 제외
+#             progresses.pop(0)
+#             speeds.pop(0)
+#
+#             # 배포 카운트 증가
+#             # 오늘 다른 작업물의 완료상태를 확인해야하니 time은 증가하지 않는다.
+#             count += 1
+#
+#         # 현재 작업이 완료되지 않았다면,
+#         else:
+#             # 이전에 완료한 작업이 있으면 배포한다.
+#             if count > 0:
+#                 answer.append(count)
+#                 count = 0 # 배포하였으니 다시 카운트는 0으로 설정
+#
+#             # 오늘 작업량에 대한 하루 증가
+#             time += 1
+#
+#     answer.append(count)
+#     return answer
 
 
 print(solution([93, 30, 55], [1, 30, 5]))
