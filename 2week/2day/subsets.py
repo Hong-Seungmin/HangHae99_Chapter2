e1a05c186fce3818a87933fe258b2e152efa5dd7
hongseungmin <globalsh1@gmail.com>
2022-03-19 19:47:55 +0900

    힌트 보고 풀었다.. 어려운 방식이다.

---
+++ 2week/2day/subsets.py
@@ -0,0 +1,32 @@
+# https://leetcode.com/problems/subsets/
+from collections import deque
+from typing import List
+
+
+# 주어진 정수리스트를 이용하여, 부분집합을 모두 구하여 반환하라.
+# 주어진 정수들은 중복되지 않으며, 구할 부분집합은 중복되지 않아야 한다.
+# Input: nums = [1,2,3]
+# Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        answer = []
+        nums_length = len(nums)
+
+        # 기본 트리거를 만들어준다.
+        queue = deque([(0, [])])
+
+        # [] 을 시작으로 1 > 2 > 3 ... 반복적으로 아이템에 아이템을 붙이는 과정이다.
+        while queue:
+            # 뽑아낸 아이템을 기준으로 확장해 나간다.
+            index, sub_nums = queue.popleft()
+            answer.append(sub_nums)
+            for i in range(index, nums_length):
+                queue.append((i + 1, sub_nums + [nums[i]]))
+
+        return answer
+
+
+sol = Solution()
+print(sol.subsets([]))
+print(sol.subsets([1, 2, 3]))
+print(sol.subsets([1, 2, 3, 4]))
