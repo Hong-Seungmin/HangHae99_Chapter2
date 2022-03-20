4ff2429e241018ad911c33b6cb647262392434c2
hongseungmin <globalsh1@gmail.com>
2022-03-20 15:22:01 +0900

    나름 쉬운 방식을 생각해내어 풀어보았다.

--- 2week/2day/subsets.py
+++ 2week/2day/subsets.py
@@ -9,19 +9,39 @@ from typing import List
 # Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        answer = []
+        answer = [[]]
         nums_length = len(nums)
 
-        # 기본 트리거를 만들어준다.
-        queue = deque([(0, [])])
+        #################################
+        ## 방법 1
+        #################################
+        # # 기본 트리거를 만들어준다.
+        # queue = deque([(0, [])])
+        #
+        # # [] 을 시작으로 1 > 2 > 3 ... 반복적으로 아이템에 아이템을 붙이는 과정이다.
+        # while queue:
+        #     # 뽑아낸 아이템을 기준으로 확장해 나간다.
+        #     index, sub_nums = queue.popleft()
+        #     answer.append(sub_nums)
+        #     for i in range(index, nums_length):
+        #         queue.append((i + 1, sub_nums + [nums[i]]))
+        #
+        #################################
+        ## 방법 1 끝
+        #################################
 
-        # [] 을 시작으로 1 > 2 > 3 ... 반복적으로 아이템에 아이템을 붙이는 과정이다.
-        while queue:
-            # 뽑아낸 아이템을 기준으로 확장해 나간다.
-            index, sub_nums = queue.popleft()
-            answer.append(sub_nums)
-            for i in range(index, nums_length):
-                queue.append((i + 1, sub_nums + [nums[i]]))
+        # nums의 길이반복 반복하며, sub_ans에 각 항목마다 nums[i]추가를 진행한다.
+        for i in range(nums_length):
+            # 사이클 기준으로 sub_ans를 깊은복사하여, 새로운 반복기준을 잡아준다.
+            sub_ans = answer.copy()
+            # 현재 sub_ans의 모든항에 nums[i]를 추가한다.
+            for node in sub_ans:
+                node = node.copy()
+                node.append(nums[i])
+
+                # nums[i]를 추가한 항목을 answer에 넣어준다.
+                # answer.append(node)
+                answer += [node]
 
         return answer
 
