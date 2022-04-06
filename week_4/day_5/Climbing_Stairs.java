ca5f0653c1b22e5a895954e5abe35c5963a4049d
Seungmin <globalsh1@gmail.com>
2022-04-06 16:43:41 +0900

    계단오르 / 피보나치와 동등한 식이다.

---
+++ week_4/day_5/Climbing_Stairs.java
@@ -0,0 +1,23 @@
+package day_5;
+
+public class Climbing_Stairs {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.climbStairs(6));
+    }
+
+    static class Solution {
+        public int climbStairs(int n) {
+            if (n < 2) return n;
+            int[] dp = new int[n];
+            dp[0] = 1;
+            dp[1] = 2;
+
+            for (int i = 2; i < n; i++) {
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+
+            return dp[n-1];
+        }
+    }
+}
