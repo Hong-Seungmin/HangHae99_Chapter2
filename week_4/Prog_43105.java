401e1685a82e6b5baff14241d661c51dbd302fea
Seungmin <globalsh1@gmail.com>
2022-04-07 13:43:15 +0900

    정수 삼각형 / 상향식 DP를 써보았다. / 매번 하향식으로만 고민했었는데.. 고정관념을 깨야한다는걸 느꼈다. / 큰문제를 작게 만드는것이 핵심이다. 루트(1개)보다는 리프(N개)가 우선이다.

--- week_4/Prog_43105.java
+++ week_4/Prog_43105.java
@@ -1,4 +1,3 @@
-
 // https://programmers.co.kr/learn/courses/30/lessons/43105
 public class Prog_43105 {
     public static void main(String[] args) {
@@ -9,20 +8,17 @@ public class Prog_43105 {
 
     static class Solution {
         public int solution(int[][] triangle) {
-            int sum = triangle[0][0];
+            int answer = 0;
 
-            int j = 0;
-            for (int i = 1; i < triangle.length; i++) {
-                System.out.println(j);
-                if (triangle[i][j] > triangle[i][j + 1]) {
-                    sum += triangle[i][j];
-                }else{
-                    sum += triangle[i][j+1];
-                    j = j + 1;
+            for (int i = triangle.length - 1; i > 0; i--) {
+                for (int j = 0; j < triangle[i].length - 1; j++) {
+                    int max = Math.max(triangle[i][j], triangle[i][j + 1]);
+                    triangle[i - 1][j] += max;
                 }
             }
+            answer = triangle[0][0];
 
-            return sum;
+            return answer;
         }
     }
 }
